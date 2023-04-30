fa2660c120ebf2f7ef4c7cbb5e3ccb90486d9aa8
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-04-30 15:14:29 +0900

    リストアプリケーション

--- src/main.go
+++ src/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
+var listData = []string{
+	"Item 1",
+	"Item 2",
+	"Item 3",
+}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("List App")
 
-	w.SetContent(widget.NewLabel("Hello Fyne!"))
+	list := widget.NewList(
+		func() int {
+			return len(listData)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(listData[i])
+		},
+	)
+
+	w.SetContent(list)
+	w.SetFixedSize(true)
+	w.Resize(fyne.NewSize(200, 200))
 	w.ShowAndRun()
 }
