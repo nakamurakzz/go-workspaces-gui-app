b8b07d842de5266ab32d0c4b4f707211d72eefae
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-05-01 09:01:59 +0900

    goroutineの関数切り出し

--- src/main.go
+++ src/main.go
@@ -7,25 +7,12 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 const instanceTabName = "Instances  "
 const settingsTabName = "Settings  "
 const workspacesTabName = "Workspaces  "
-
-func createStatusLabel(status string) *widget.Label {
-	// var textColor color.Color
-	// switch status {
-	// case "running":
-	// 	textColor = color.RGBA{0, 255, 0, 255}
-	// case "stopped":
-	// 	textColor = color.RGBA{255, 0, 0, 255}
-	// default:
-	// 	textColor = color.RGBA{128, 128, 128, 255}
-	// }
-	return widget.NewLabelWithStyle(status, fyne.TextAlignLeading, fyne.TextStyle{})
-}
+const updateInterval = 20 * time.Second
 
 func main() {
 	settings, err := LoadSettings()
@@ -36,27 +23,14 @@ func main() {
 
 	instances := []*Instance{}
 	workspaces := []*Workspace{}
-	if settings.GetActiveProfile() == "" {
-		instances = []*Instance{}
-		workspaces = []*Workspace{}
-	} else {
-		instances, err = getEC2Instances(settings.GetActiveProfile())
-		if err != nil {
-			log.Println("Error:", err)
-			return
-		}
-		workspaces, err = getWorkspaces(settings.GetActiveProfile())
-		if err != nil {
-			log.Println("Error:", err)
-			return
-		}
-	}
+	instances = []*Instance{}
+	workspaces = []*Workspace{}
 
 	myApp := app.New()
 	myWindow := myApp.NewWindow("EC2 Instances")
 
-	ec2ListScreen := createEC2ListView(instances, settings.GetActiveProfile())
-	workspaceListScreen := createWorkSpacesListView(workspaces, settings.GetActiveProfile())
+	ec2ListScreen := createEC2ListView(instances, "")
+	workspaceListScreen := createWorkSpacesListView(workspaces, "")
 	settingsScreen := createSettingsScreen(myApp, settings)
 	tabs := container.NewAppTabs(
 		container.NewTabItem(settingsTabName, settingsScreen),
@@ -68,14 +42,15 @@ func main() {
 
 	tabs.OnSelected = func(item *container.TabItem) {
 		activeFeature := settings.GetActiveFeature()
+		activeProfile := settings.GetActiveProfile()
 		if item.Text == instanceTabName {
 			if settings.GetActiveProfile() != "" {
-				updateInstanceStatus(ec2ListScreen, settings.GetActiveProfile(), activeFeature.EC2)
+				updateInstanceStatus(ec2ListScreen, activeProfile, activeFeature.EC2)
 			}
 		}
 		if item.Text == workspacesTabName {
 			if settings.GetActiveProfile() != "" {
-				updateWorkspacesStatus(workspaceListScreen, settings.GetActiveProfile(), activeFeature.WorkSpace)
+				updateWorkspacesStatus(workspaceListScreen, activeProfile, activeFeature.WorkSpace)
 			}
 		}
 	}
@@ -84,16 +59,19 @@ func main() {
 	myWindow.SetContent(tabs)
 	myWindow.Resize(fyne.NewSize(1200, 600))
 
-	ticker := time.NewTicker(20 * time.Second)
-	go func(settings *Settings) {
-		for range ticker.C {
-			if settings.GetActiveProfile() != "" {
-				activeFeature := settings.GetActiveFeature()
-				updateInstanceStatus(ec2ListScreen, settings.GetActiveProfile(), activeFeature.EC2)
-				updateWorkspacesStatus(workspaceListScreen, settings.GetActiveProfile(), activeFeature.WorkSpace)
-			}
-		}
-	}(settings)
+	go updateScreen(settings, ec2ListScreen, workspaceListScreen)
 
 	myWindow.ShowAndRun()
+}
+
+// 定期的にインスタンスの状態を更新する
+func updateScreen(settings *Settings, ec2ListScreen *fyne.Container, workspaceListScreen *fyne.Container) {
+	ticker := time.NewTicker(updateInterval)
+	for range ticker.C {
+		if settings.GetActiveProfile() != "" {
+			activeFeature := settings.GetActiveFeature()
+			updateInstanceStatus(ec2ListScreen, settings.GetActiveProfile(), activeFeature.EC2)
+			updateWorkspacesStatus(workspaceListScreen, settings.GetActiveProfile(), activeFeature.WorkSpace)
+		}
+	}
 }
